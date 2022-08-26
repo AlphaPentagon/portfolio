762f4c801d04e3ef96617954e673452ce79aa127
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-08-26 11:49:22 +0100

    various content changes

--- portfolio/src/components/AboutMe/AboutMe.js
+++ portfolio/src/components/AboutMe/AboutMe.js
@@ -3,73 +3,120 @@ import { useState } from "react";
 
 export function AboutMe() {
   const [active, setActive] = useState("1");
-  const [content, setContent] = useState(
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus esse
-      minima iusto provident eveniet, rem illum ipsa. Accusantium, veniam dolore
-      laboriosam omnis consequatur, quod voluptas, fuga aspernatur labore
-      laborum maxime.
-    </p>
-  );
+  const [content, setContent] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
   function handleClick(e, content) {
     setActive(e.target.id);
+    setModalOpen(true);
     switch (content) {
       default:
         break;
       case "general":
         const general = (
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Repellendus esse minima iusto provident eveniet, rem illum ipsa.
-            Accusantium, veniam dolore laboriosam omnis consequatur, quod
-            voluptas, fuga aspernatur labore laborum maxime.
-          </p>
+          <>
+            <p>
+              I'm a junior software developer with a passion for problem solving
+              and an enthusiasm for continuous learning.
+            </p>
+          </>
         );
         setContent(general);
+        break;
+      case "story":
+        const story = (
+          <>
+            <p>
+              I was an IT Support Technician for 7 years, but grew tired of
+              telling people to "turn it off and on".
+            </p>
+            <p>
+              I had a desire to learn more about tech and challenge myself, so I
+              started teaching myself web development in the evenings using free
+              resouces on the internet.
+            </p>
+            <p>
+              This sparked a passion and I knew it was something I wanted to
+              move into as a career. Knowing that I would benefit from some
+              structured learning, I discovered the School of Code free bootcamp
+              and applied.
+            </p>
+            <p>
+              In March 2022 I left my job, moved 4 hours across country with my
+              wife and daughter to be nearer to family and started my new
+              journey.
+            </p>
+            <p>
+              Now after 16 weeks of intensive and accelerated learning, I have
+              gained strong technical skills across a variety of full stack
+              technology, as well as invaluable experience of working within
+              diverse agile software engineering teams, and I am excited to put
+              it all into practise in my first junior role.
+            </p>
+          </>
+        );
+        setContent(story);
         break;
       case "experience":
         const experience = (
           <p>
-            Odio corrupti optio deleniti rem inventore sed, autem nam nulla
-            totam suscipit culpa, dicta magni repellat obcaecati vitae veritatis
-            laudantium soluta quidem omnis sunt ad recusandae quaerat nobis
-            quis. Sunt!
+            I have just graduated from the School of Code bootcamp. After 16
+            weeks of intensive & accelerated learning, I have gained strong
+            technical skills across a variety of full stack technology, as well
+            as invaluable experience of working within diverse agile software
+            engineering teams, and I am excited to put it all into practise in
+            my first junior role.
           </p>
         );
         setContent(experience);
         break;
       case "interests":
         const interests = (
-          <p>
-            Inventore neque libero aperiam sequi obcaecati eveniet voluptatibus
-            velit voluptates unde culpa qui, atque ratione, magni ut, corrupti
-            delectus ullam ab veritatis! In cupiditate obcaecati voluptates
-            porro vitae debitis voluptatibus.
-          </p>
+          <>
+            <p>
+              When I'm not being a climbing frame for my young daughter, I
+              enjoy: writing, recording and playing music, starting (but rarely
+              finishing) videogames, losing to my wife at cards and spending
+              time with friends and family.
+            </p>
+            <p>
+              I also enjoy attending gigs and music festivals and I am
+              passionate about wellbeing and mental health.
+            </p>
+          </>
         );
         setContent(interests);
         break;
     }
   }
 
+  function handleClose() {
+    setModalOpen(false);
+  }
+
   // when button is clicked, apply active styling
   // use an onclick to trigger and dynamically change class
   return (
-    <section className="aboutMe">
-      <div className="image-container">
-        <img src="images\Craig.jpg" alt="#" />
-        {/* <h3>Craig Summers</h3> */}
-        {/* <p>FULL STACK DEVELOPER</p> */}
-      </div>
-      <div className="info-container">
-        <h2>Hi, I'm Craig</h2>
-        {content}
-      </div>
-      <p className="aboutMe-more heartbeat">
+    <section className="aboutMeSection">
+      <div className="aboutMeContainer">
+        {/* <p className="aboutMe-more heartbeat">
         Click to find out more <span className="aboutMe-arrow">&#8594;</span>
-      </p>
+      </p> */}
+
+        <div className="image-container">
+          <img src="images\Craig2.jfif" alt="#" />
+          {/* <h3>Craig Summers</h3> */}
+          {/* <p>FULL STACK DEVELOPER</p> */}
+        </div>
+        <div>
+          <h2>Hi, I'm Craig</h2>
+          <p>
+            I'm a junior software developer with a passion for problem solving
+            and an enthusiasm for continuous learning.
+          </p>
+        </div>
+      </div>
       <div className="aboutMe-button-container">
-        <button
+        {/* <button
           className={
             active === "1" ? "aboutMe-button-active" : "aboutMe-button"
           }
@@ -79,30 +126,49 @@ export function AboutMe() {
           id={"1"}
         >
           General
-        </button>
+        </button> */}
         <button
           className={
             active === "2" ? "aboutMe-button-active" : "aboutMe-button"
           }
           onClick={(e) => {
-            handleClick(e, "experience");
+            handleClick(e, "story");
           }}
           id={"2"}
         >
-          Experience
+          My Story
         </button>
         <button
           className={
             active === "3" ? "aboutMe-button-active" : "aboutMe-button"
           }
           onClick={(e) => {
-            handleClick(e, "interests");
+            handleClick(e, "experience");
           }}
           id={"3"}
+        >
+          Experience
+        </button>
+        <button
+          className={
+            active === "4" ? "aboutMe-button-active" : "aboutMe-button"
+          }
+          onClick={(e) => {
+            handleClick(e, "interests");
+          }}
+          id={"4"}
         >
           Interests
         </button>
       </div>
+      {modalOpen && (
+        <>
+          <div className="info-container">
+            <button onClick={handleClose}>Close</button>
+            {content}
+          </div>
+        </>
+      )}
     </section>
   );
 }
